92c5881903b62f9b13b69e29d99567c4c95a5c7d
Paola Arredondo <p.arredondo@fetchrewards.com>
2021-06-23 17:44:22 -0400

    app with bugs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Table, OverlayTrigger, Tooltip, Button } from 'react-bootstrap';
 import './App.css';
+import data from './data';
+
 
 function App() {
+  const [breedImages, setBreedImages] = useState([]);
+  const [currentBreed, setCurrentBreed] = useState({});
+
+  const loadBreed = (breedRoute, breedInfo) => {
+    try {
+      const response = axios.get(`https://dog.ceo/api/breed/${breedRoute}/images`)
+      setBreedImages(response.data.message);
+      setCurrentBreed(breedInfo);
+      
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Doggies</h1>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Breed</th>
+            <th>Life Expectancy</th>
+            <th>Size</th>
+            <th>Popularity</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((breed) => (
+            <tr key={breed.breedRoute}>
+              <td>{breed.breedName}</td>
+              <td>{breed.lifeExpectancy}</td>
+              <td>{breed.size}</td>
+              <td>{breed.popularity}</td>
+              <td>
+                <OverlayTrigger
+                    overlay={
+                        <Tooltip>
+                            View breed details
+                        </Tooltip>
+                    }
+                    delay={250}
+                    placement="left"
+                >
+                    <Button onClick={loadBreed(breed.breedRoute, breed)}>
+                        <i className="fas fa-eye" />
+                    </Button>
+                </OverlayTrigger>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+
+      <h1>{currentBreed.breedName}</h1>
+      <p>Life Expectancy: {currentBreed.lifeExpectancy}</p>
+      <p>Size: {currentBreed.size}</p>
+      <p>Popularity: {currentBreed.popularity}</p>
+      <p>Temperament: {currentBreed.temperament}</p>
+      <p>About:</p>
+      <p>{currentBreed.about}</p>
+
+      {breedImages.map((url) => (
+        <img key={url} alt="dog" src={url}/>
+      ))}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,85 @@
+const data = [
+    {
+        breedName: 'Golden Retriever',
+        breedRoute: 'retriever/golden',
+        lifeExpectancy: '10-12 years',
+        size: 'medium',
+        popularity: 3,
+        temperament: 'Intelligent, Friendly, Reliable, Kind, Trustworthy, Confident',
+        about: `The Golden Retriever is a sturdy, muscular dog of medium size, famous for the dense,
+        lustrous coat of gold that gives the breed its name. The broad head, with its friendly and
+        intelligent eyes, short ears, and straight muzzle, is a breed hallmark. In motion, Goldens
+        move with a smooth, powerful gait, and the feathery tail is carried, as breed fanciers say,
+        with a “merry action.”
+        The most complete records of the development of the Golden Retriever are included in the
+        record books that were kept from 1835 until about 1890 by the gamekeepers at the Guisachan
+        (pronounced Gooeesicun) estate of Lord Tweedmouth at Inverness-Shire, Scotland. These records
+        were released to public notice in Country Life in 1952, when Lord Tweedmouth’s great-nephew,
+        the sixth Earl of Ilchester, historian and sportsman, published material that had been left by his ancestor.
+        They provided factual confirmation to the stories that had been handed down through generations.`
+    },
+    {
+        breedName: 'Bernese Mountain Dog',
+        breedRoute: 'mountain/bernese',
+        lifeExpectancy: '7-10 years',
+        size: 'large',
+        popularity: 22,
+        temperament: 'Good-Natured, Calm, Strong',
+        about: `The Bernese Mountain Dog is a large, sturdy worker who can stand over
+        27 inches at the shoulder. The thick, silky, and moderately long coat is tricolored:
+        jet black, clear white, and rust. The distinctive markings on the coat and face
+        are breed hallmarks and, combined with the intelligent gleam in the dark eyes,
+        add to the Berner’s aura of majestic nobility. A hardy dog who thrives in cold weather,
+        the Berner’s brain and brawn helped him multitask on the farms and pastures of
+        Switzerland. Berners get along with the entire family and are particularly gentle
+        with children, but they will often become more attached to one lucky human.
+        Berners are imposing but not threatening, and they maintain an aloof dignity
+        with strangers.`
+    },
+    {
+        breedName: 'Doberman Pinscher',
+        breedRoute: 'doberman',
+        lifeExpectancy: '10-12 years',
+        size: 'medium',
+        popularity: 17,
+        temperament: 'Loyal, Fearless, Alert',
+        about: `Dobermans are compactly-built dogs—muscular, fast, and powerful—standing
+        between 24 to 28 inches at the shoulder. The body is sleek but substantial, and
+        is covered with a glistening coat of black, blue, red, or fawn, with rust markings.
+        These elegant qualities, combined with a noble, wedge-shaped head and an easy,
+        athletic way of moving have earned Dobermans a reputation as royalty in the canine
+        kingdom. A well-conditioned Doberman on patrol will deter all but the most foolish
+        intruder.`
+    },
+    {
+        breedName: 'German Shepherd',
+        breedRoute: 'germanshepherd',
+        lifeExpectancy: '12-14 years',
+        size: 'medium',
+        popularity: 2,
+        temperament: 'Confident, Courageous',
+        about: `German Shepherd Dogs can stand as high as 26 inches at the shoulder and,
+        when viewed in outline, presents a picture of smooth, graceful curves rather than
+        angles. The natural gait is a free-and-easy trot, but they can turn it up a notch
+        or two and reach great speeds.`
+    },
+    {
+        breedName: 'Australian Shepherd',
+        breedRoute: 'australian/shepherd',
+        lifeExpectancy: '12-15 years',
+        size: 'medium',
+        popularity: 18,
+        temperament: 'Smart, Work-Oriented, Exuberant',
+        about: `The Australian Shepherd, the cowboy’s herding dog of choice, is a medium-sized
+        worker with a keen, penetrating gaze in the eye. Aussie coats offer different looks,
+        including merle (a mottled pattern with contrasting shades of blue or red).
+        In all ways, they’re the picture of rugged and agile movers of stock. Aussies exhibit
+        an irresistible impulse to herd, anything: birds, dogs, kids. This strong work drive
+        can make Aussies too much dog for a sedentary pet owner. Aussies are remarkably intelligent,
+        quite capable of hoodwinking an unsuspecting novice owner. In short, this isn’t the
+        pet for everyone. But if you’re looking for a brainy, tireless, and trainable partner
+        for work or sport, your search might end here.`
+    },
+];
+
+export default data
